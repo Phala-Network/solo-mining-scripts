80a089a347feeb0bbd1063f37a80e3ed20dcf1ec
YiFeng <jiangyifeng2016@gmail.com>
2021-01-03 13:51:31 +0800

    fix curl command not find

--- scripts/install_phala.sh
+++ scripts/install_phala.sh
@@ -12,7 +12,7 @@ install_depenencies()
     log_info "----------Install depenencies----------"
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    apt-get install -y docker-ce docker-ce-cli containerd.io jq
+    apt-get install -y docker-ce docker-ce-cli containerd.io jq curl
     if [ $? -ne 0 ]; then
         log_err "Install depenencies failed"
         exit 1
