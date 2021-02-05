8989cd64f311bf04fcd2f5b48bd11986cd668294
YiFeng <jiangyifeng2016@gmail.com>
2021-02-05 22:42:45 +0800

    Update dcap and isgx downloading url

--- scripts/cn/install_phala.sh
+++ scripts/cn/install_phala.sh
@@ -160,15 +160,15 @@ install()
 {
 	release=$(lsb_release -r | grep -o "[0-9]*\.[0-9]*")
 	if [ x"$release" = x"18.04" ]; then
-		dcap_driverbin=sgx_linux_x64_driver_1.36.2.bin
-		dcap_driverurl=https://download.01.org/intel-sgx/sgx-dcap/1.9/linux/distro/ubuntu18.04-server/sgx_linux_x64_driver_1.36.2.bin
-		isgx_driverbin=sgx_linux_x64_driver_2.11.0_4505f07.bin
-		isgx_driverurl=https://download.01.org/intel-sgx/latest/linux-latest/distro/ubuntu18.04-server/sgx_linux_x64_driver_2.11.0_4505f07.bin
+		dcap_driverbin=sgx_linux_x64_driver_1.41.bin
+		dcap_driverurl=https://download.01.org/intel-sgx/latest/dcap-latest/linux/distro/ubuntu18.04-server/sgx_linux_x64_driver_1.41.bin
+		isgx_driverbin=sgx_linux_x64_driver_2.11.0_0373e2e.bin
+		isgx_driverurl=https://download.01.org/intel-sgx/latest/linux-latest/distro/ubuntu18.04-server/sgx_linux_x64_driver_2.11.0_0373e2e.bin
 	elif [ x"$release" = x"20.04" ]; then
-		dcap_driverbin=sgx_linux_x64_driver_1.36.2.bin
-		dcap_driverurl=https://download.01.org/intel-sgx/sgx-dcap/1.9/linux/distro/ubuntu20.04-server/sgx_linux_x64_driver_1.36.2.bin
-		isgx_driverbin=sgx_linux_x64_driver_2.11.0_4505f07.bin
-		isgx_driverurl=https://download.01.org/intel-sgx/latest/linux-latest/distro/ubuntu20.04-server/sgx_linux_x64_driver_2.11.0_4505f07.bin
+		dcap_driverbin=sgx_linux_x64_driver_1.41.bin
+		dcap_driverurl=https://download.01.org/intel-sgx/latest/dcap-latest/linux/distro/ubuntu20.04-server/sgx_linux_x64_driver_1.41.bin
+		isgx_driverbin=sgx_linux_x64_driver_2.11.0_0373e2e.bin
+		isgx_driverurl=https://download.01.org/intel-sgx/latest/linux-latest/distro/ubuntu20.04-server/sgx_linux_x64_driver_2.11.0_0373e2e.bin
 	else
 		log_err "----------系统版本不支持----------"
 		exit 1
