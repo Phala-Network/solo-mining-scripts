c2acecd6bfde72210322b4ebf6568983e64570f9
YiFeng <jiangyifeng2016@gmail.com>
2021-02-08 12:13:50 +0800

    Fixed /dev/sgx and /dev/isgx not find

--- scripts/cn/start.sh
+++ scripts/cn/start.sh
@@ -4,7 +4,7 @@ start_phala_node()
 {
 	log_info "---------启动 phala node----------"
 	local node_name=$(cat $basedir/config.json | jq -r '.nodename')
-	docker run -ti --rm --name phala-node -d -e NODE_NAME=$node_name -p 9933:9933 -p 9944:9944 -p 30333:30333 -v $HOME/phala-node-data:/root/data phalanetwork/phala-poc3-node
+	docker run -ti --rm --name phala-node -d -e NODE_NAME=$node_name -p 9933:9933 -p 9944:9944 -p 30333:30333 -v $HOME/phala-node-data:/root/data swr.cn-east-3.myhuaweicloud.com/phala/phala-poc3-node
 
 	if [ $? -ne 0 ]; then
 		log_err "----------启动 phala node 失败-------------"
@@ -29,29 +29,29 @@ start_phala_node()
 
 start_phala_node_debug()
 {
-        log_info "---------启动 phala node----------"
-        local node_name=$(cat $basedir/config.json | jq -r '.nodename')
-	docker run -ti --rm --name phala-node -e NODE_NAME=$node_name -p 9933:9933 -p 9944:9944 -p 30333:30333 -v $HOME/phala-node-data:/root/data phalanetwork/phala-poc3-node
+	log_info "---------启动 phala node----------"
+	local node_name=$(cat $basedir/config.json | jq -r '.nodename')
+	docker run -ti --rm --name phala-node -e NODE_NAME=$node_name -p 9933:9933 -p 9944:9944 -p 30333:30333 -v $HOME/phala-node-data:/root/data swr.cn-east-3.myhuaweicloud.com/phala/phala-poc3-node
 
-        if [ $? -ne 0 ]; then
-                log_err "----------启动 phala node 失败-------------"
-                exit 1
-        fi
+	if [ $? -ne 0 ]; then
+			log_err "----------启动 phala node 失败-------------"
+			exit 1
+	fi
 
-        log_info "等待node节点同步区块高度"
-        sleep 30
-        while true ; do
-                local ipaddr=$(cat $basedir/config.json | jq -r '.ipaddr')
-                local block_json=$(curl -sH "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "system_syncState", "params":[]}' http://$ipaddr:9933)
-                local node_block=$(echo $block_json | jq -r '.result.currentBlock')
-                local hightest_block=$(echo $block_json | jq -r '.result.highestBlock')
-                if [ x"$node_block" == x"$hightest_block" ] && [ x"$hightest_block" > x"10" ]; then
-                        log_success "phala-node 同步完成"
-                        break
-                fi
-                log_info "同步进度： 节点高度（$node_block），网络高度（$hightest_block）"
-                sleep 30
-        done
+	log_info "等待node节点同步区块高度"
+	sleep 30
+	while true ; do
+			local ipaddr=$(cat $basedir/config.json | jq -r '.ipaddr')
+			local block_json=$(curl -sH "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "system_syncState", "params":[]}' http://$ipaddr:9933)
+			local node_block=$(echo $block_json | jq -r '.result.currentBlock')
+			local hightest_block=$(echo $block_json | jq -r '.result.highestBlock')
+			if [ x"$node_block" == x"$hightest_block" ] && [ x"$hightest_block" > x"10" ]; then
+					log_success "phala-node 同步完成"
+					break
+			fi
+			log_info "同步进度： 节点高度（$node_block），网络高度（$hightest_block）"
+			sleep 30
+	done
 }
 
 start_phala_pruntime()
@@ -61,9 +61,9 @@ start_phala_pruntime()
 	local res_sgx=$(ls /dev | grep -w sgx)
 	local res_isgx=$(ls /dev | grep -w isgx)
 	if [ x"$res_sgx" == x"sgx" ] && [ x"$res_isgx" == x"" ]; then
-		docker run -d -ti --rm --name phala-pruntime -p 8000:8000 -v $HOME/phala-pruntime-data:/root/data --device /dev/sgx/enclave --device /dev/sgx/provision phalanetwork/phala-poc3-pruntime
+		docker run -d -ti --rm --name phala-pruntime -p 8000:8000 -v $HOME/phala-pruntime-data:/root/data --device /dev/sgx/enclave --device /dev/sgx/provision swr.cn-east-3.myhuaweicloud.com/phala/phala-poc3-pruntime
 	elif [ x"$res_isgx" == x"isgx" ] && [ x"$res_sgx" == x"" ]; then
-		docker run -d -ti --rm --name phala-pruntime -p 8000:8000 -v $HOME/phala-pruntime-data:/root/data --device /dev/isgx phalanetwork/phala-poc3-pruntime
+		docker run -d -ti --rm --name phala-pruntime -p 8000:8000 -v $HOME/phala-pruntime-data:/root/data --device /dev/isgx swr.cn-east-3.myhuaweicloud.com/phala/phala-poc3-pruntime
 	else
 		log_err "----------sgx/dcap 驱动没有安装----------"
 		exit 1
@@ -82,9 +82,9 @@ start_phala_pruntime_debug()
 	local res_sgx=$(ls /dev | grep -w sgx)
 	local res_isgx=$(ls /dev | grep -w isgx)
 	if [ x"$res_sgx" == x"sgx" ] && [ x"$res_isgx" == x"" ]; then
-		docker run -ti --rm --name phala-pruntime -p 8000:8000 -v $HOME/phala-pruntime-data:/root/data --device /dev/sgx/enclave --device /dev/sgx/provision phalanetwork/phala-poc3-pruntime
+		docker run -ti --rm --name phala-pruntime -p 8000:8000 -v $HOME/phala-pruntime-data:/root/data --device /dev/sgx/enclave --device /dev/sgx/provision swr.cn-east-3.myhuaweicloud.com/phala/phala-poc3-pruntime
 	elif [ x"$res_isgx" == x"isgx" ] && [ x"$res_sgx" == x"" ]; then
-		docker run -ti --rm --name phala-pruntime -p 8000:8000 -v $HOME/phala-pruntime-data:/root/data --device /dev/isgx phalanetwork/phala-poc3-pruntime
+		docker run -ti --rm --name phala-pruntime -p 8000:8000 -v $HOME/phala-pruntime-data:/root/data --device /dev/isgx swr.cn-east-3.myhuaweicloud.com/phala/phala-poc3-pruntime
 	else
 		log_err "----------sgx/dcap 驱动没有安装----------"
 		exit 1
@@ -101,7 +101,7 @@ start_phala_phost()
 	log_info "----------启动phost----------"
 	local ipaddr=$(cat $basedir/config.json | jq -r '.ipaddr')
 	local mnemonic=$(cat $basedir/config.json | jq -r '.mnemonic')
-	docker run -d -ti --rm --name phala-phost -e PRUNTIME_ENDPOINT="http://$ipaddr:8000" -e PHALA_NODE_WS_ENDPOINT="ws://$ipaddr:9944" -e MNEMONIC="$mnemonic" -e EXTRA_OPTS="-r" phalanetwork/phala-poc3-phost
+	docker run -d -ti --rm --name phala-phost -e PRUNTIME_ENDPOINT="http://$ipaddr:8000" -e PHALA_NODE_WS_ENDPOINT="ws://$ipaddr:9944" -e MNEMONIC="$mnemonic" -e EXTRA_OPTS="-r" swr.cn-east-3.myhuaweicloud.com/phala/phala-poc3-phost
 
 	if [ $? -ne 0 ]; then
 		log_err "----------启动phost失败----------"
@@ -114,7 +114,7 @@ start_phala_phost_debug()
         log_info "----------启动phost----------"
         local ipaddr=$(cat $basedir/config.json | jq -r '.ipaddr')
         local mnemonic=$(cat $basedir/config.json | jq -r '.mnemonic')
-	docker run -ti --rm --name phala-phost -e PRUNTIME_ENDPOINT="http://$ipaddr:8000" -e PHALA_NODE_WS_ENDPOINT="ws://$ipaddr:9944" -e MNEMONIC="$mnemonic" -e EXTRA_OPTS="-r" phalanetwork/phala-poc3-phost
+	docker run -ti --rm --name phala-phost -e PRUNTIME_ENDPOINT="http://$ipaddr:8000" -e PHALA_NODE_WS_ENDPOINT="ws://$ipaddr:9944" -e MNEMONIC="$mnemonic" -e EXTRA_OPTS="-r" swr.cn-east-3.myhuaweicloud.com/phala/phala-poc3-phost
 
         if [ $? -ne 0 ]; then
                 log_err "----------启动phost失败----------"
