788d0fcd83ec5ae1fc2f7eb05a33c0f808e794ad
wonderfulstudy <jiangyifeng2016@gmail.com>
2021-07-11 15:30:35 +0800

    Update reportsystemlog.sh

--- reportsystemlog.sh
+++ reportsystemlog.sh
@@ -3,9 +3,6 @@ apt-get install -y zip
 mkdir systemlog
 ti=$(date +%s)
 dmidecode > ./systemlog/system$ti.inf
-docker logs phala-node --tail 50000 > ./systemlog/node$ti.inf
-docker logs phala-phost --tail 50000 > ./systemlog/phost$ti.inf
-docker logs phala-pruntime --tail 50000 > ./systemlog/pruntime$ti.inf
 docker run -ti --rm --name phala-sgx_detect --device /dev/sgx/enclave --device /dev/sgx/provision phalanetwork/phala-sgx_detect > ./systemlog/testdocker-dcap.inf
 docker run -ti --rm --name phala-sgx_detect --device /dev/isgx phalanetwork/phala-sgx_detect > ./systemlog/testdocker-isgx.inf
 zip -r systemlog$ti.zip systemlog/*
