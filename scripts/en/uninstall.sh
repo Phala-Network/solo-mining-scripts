ac2d0a88af2ba08d6333913c787dd711932ceeb8
wonderfulstudy <jiangyifeng2016@gmail.com>
2021-08-02 18:32:22 +0800

    update uninstall.sh

--- scripts/en/uninstall.sh
+++ scripts/en/uninstall.sh
@@ -7,7 +7,9 @@ uninstall()
 	docker-compose rm phala-node phala-pruntime phala-pherry
 	docker image rm phala-dev-node phala-dev-pruntime phala-dev-pherry # 合并到main时，更新image名字
 	remove_dirver
-	rm -rf $installdir
+	local node_data=$(awk -F '[=:]' 'NR==4 {print $2}' $installdir/.env)
+	local pruntime_data=$(awk -F '[=:]' 'NR==5 {print $2}' $installdir/.env)
+	rm -rf $installdir $node_data $pruntime_data
 	rm /usr/bin/phala
 
 	log_success "---------------Uninstall phala node sucess---------------"
