81a98cdb13cc75b0e6f6ddbcf5f50cd30f1aa920
wonderfulstudy <jiangyifeng2016@gmail.com>
2021-08-05 22:22:11 +0800

    update container name

--- scripts/en/status.sh
+++ scripts/en/status.sh
@@ -17,7 +17,7 @@ status()
 		local node_block=$(curl -sH "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "system_syncState", "params":[]}' http://0.0.0.0:9933 | jq '.result.currentBlock')
 		local publickey=$(curl -X POST -sH "Content-Type: application/json" -d '{"input": {}, "nonce": {}}' http://0.0.0.0:8000/get_info | jq '.payload|fromjson.public_key' | sed 's/\"//g' | sed 's/^/0x/')
 
-		check_docker_status kphala-dev-node
+		check_docker_status khala-dev-node
 		local res=$?
 		if [ $res -eq 0 ]; then
 			node_status="running"
