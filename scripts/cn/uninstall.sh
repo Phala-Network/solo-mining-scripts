bb9dc35cf631b508d6d6a7eea2e11470ff392f8d
wonderfulstudy <jiangyifeng2016@gmail.com>
2021-08-05 18:46:55 +0800

    update image name

--- scripts/cn/uninstall.sh
+++ scripts/cn/uninstall.sh
@@ -4,8 +4,8 @@ uninstall()
 {
 	cd $installdir
 	docker-compose stop
-	docker-compose rm phala-node phala-pruntime phala-pherry
-	docker image rm phala-dev-node phala-dev-pruntime phala-dev-pherry # 合并到main时，更新image名字
+	docker-compose rm khala-dev-node phala-dev-pruntime phala-dev-pherry
+	docker image rm khala-dev-node phala-dev-pruntime phala-dev-pherry # 合并到main时，更新image名字
 	remove_dirver
 	local node_data=$(awk -F '[=:]' 'NR==4 {print $2}' $installdir/.env)
 	local pruntime_data=$(awk -F '[=:]' 'NR==5 {print $2}' $installdir/.env)
