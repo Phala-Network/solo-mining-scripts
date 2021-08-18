5b9c88b0855c8e69c32a643cc97a25026714a15f
wonderfulstudy <jiangyifeng2016@gmail.com>
2021-08-18 18:10:25 +0800

    checking the kernel version before phala config

--- scripts/cn/stop.sh
+++ scripts/cn/stop.sh
@@ -18,32 +18,32 @@ stop()
 		"")
 			for container_name in phala-node phala-pruntime phala-pherry phala-pruntime-bench khala-node
 			do
-				if [ ! -z $(docker ps -qf "name=$container_name") ]; then docker container rm --force $container_name; fi
+				if [ ! -z $(docker container ls -q -f "name=$container_name") ]; then docker container rm --force $container_name; fi
 			done
 			;;
 		node)
-			if [ ! -z $(docker ps -qf "name=phala-node") ]; then
+			if [ ! -z $(docker container ls -q -f "name=phala-node") ]; then
 				docker container rm --force phala-node
 			else
 				log_info "----------phala-node容器已经停止----------"
 			fi
 			;;
 		pruntime)
-			if [ ! -z $(docker ps -qf "name=phala-pruntime") ]; then
+			if [ ! -z $(docker container ls -q -f "name=phala-pruntime") ]; then
 				docker container rm --force phala-pruntime
 			else
 				log_info "----------phala-pruntime容器已经停止----------"
 			fi
 			;; 
 		pherry)
-			if [ ! -z $(docker ps -qf "name=phala-pherry") ]; then
+			if [ ! -z $(docker container ls -q -f "name=phala-pherry") ]; then
 				docker container rm --force phala-pherry
 			else
 				log_info "----------phala-pherry容器已经停止----------"
 			fi
 			;;
 		bench)
-			if [ ! -z $(docker ps -qf "name=phala-pruntime-bench") ]; then
+			if [ ! -z $(docker container ls -q -f "name=phala-pruntime-bench") ]; then
 				docker container rm --force phala-pruntime-bench
 			else
 				log_info "----------phala-pruntime-bench容器已经停止----------"
