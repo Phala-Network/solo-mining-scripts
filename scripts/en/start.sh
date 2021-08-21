ad30ac2590c36072d99189a769488df1378a481b
wonderfulstudy <jiangyifeng2016@gmail.com>
2021-08-21 14:48:53 +0800

    update the version to 0.1.4

--- scripts/en/start.sh
+++ scripts/en/start.sh
@@ -2,25 +2,23 @@
 
 function start()
 {
+	if [ ! -L /dev/sgx/enclave ]&&[ ! -L /dev/sgx/provision ]&&[ ! -c /dev/sgx_enclave ]&&[ ! -c /dev/sgx_provision ]&&[ ! -c /dev/isgx ]; then
+		install
+	elif ! type jq curl wget unzip zip docker docker-compose node yq dkms > /dev/null 2>&1; then
+		install_depenencies
+	fi
+
 	local node_name=$(cat $installdir/.env | grep 'NODE_NAME' | awk -F "=" '{print $NF}')
 	local cores=$(cat $installdir/.env | grep 'CORES' | awk -F "=" '{print $NF}')
 	local mnemonic=$(cat $installdir/.env | grep 'MNEMONIC' | awk -F "=" '{print $NF}')
 	local pool_address=$(cat $installdir/.env | grep 'OPERATOR' | awk -F "=" '{print $NF}')
-	if ! type docker docker-compose node yq jq curl wget unzip zip >/dev/null 2>&1; then
-		log_err "----------Lack of important dependent tools, please execute 'sudo phala install' to reinstall！----------"
-		exit 1
-	fi
-
-	if [ ! -c /dev/sgx_enclave -a ! -c /dev/sgx_provision ]&&[ ! -c /dev/isgx ]; then
-		log_err "----------The dcap/isgx driver is not installed, please execute the 'sudo phala install dcap'/'sudo phala install isgx' command to install!----------"
-		exit 1
-	fi
-
 	if [ -z "$node_name" ]||[ -z "$cores" ]||[ -z "$mnemonic" ]||[ -z "$pool_address" ]; then
-		log_err "----------The node is not configured, or the important configuration is lost, start configuring the node!----------"
-		phala config set
+		log_err "----------The node is not configured, start configuring the node!----------"
+		config set
 	fi
 	cd $installdir
 	docker-compose up -d
-	docker run -dti --rm --name khala-node -e NODE_NAME=$node_name -e NODE_ROLE=MINER -p 40333:30333 -p 40334:30334 -v /var/khala-dev-node:/root/data phalanetwork/khala-node
+	if [ ! -z $(docker container ls -q -f "name=khala-node") ]; then
+		docker run -dti --rm --name khala-node -e NODE_NAME=$node_name -e NODE_ROLE=MINER -p 40333:30333 -p 40334:30334 -v /var/khala-dev-node:/root/data phalanetwork/khala-node
+	fi
 }
