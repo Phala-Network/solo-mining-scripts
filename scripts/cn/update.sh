d051d1122910df197562cbf1a333642e7f5e5c76
wonderfulstudy <jiangyifeng2016@gmail.com>
2021-08-21 13:14:18 +0800

    update the version to 0.1.4

--- scripts/cn/update.sh
+++ scripts/cn/update.sh
@@ -47,32 +47,26 @@ function update_clean()
 		if [ ! -z $(docker container ls -q -f "name=$container_name") ]; then
 			docker container stop $container_name
 			docker container rm --force $container_name
+			case $container_name in
+				phala-node)
+					docker image rm $(awk -F "=" 'NR==1 {print $2}' $installdir/.env)
+					if [ -d $(awk -F '[=:]' 'NR==4 {print $2}' $installdir/.env) ]; then rm -rf $(awk -F '[=:]' 'NR==4 {print $2}' $installdir/.env);fi
+					;;
+				phala-pruntime) 
+					docker image rm $(awk -F "=" 'NR==2 {print $2}' $installdir/.env)
+					if [ -d $(awk -F '[=:]' 'NR==5 {print $2}' $installdir/.env) ]; then rm -rf $(awk -F '[=:]' 'NR==5 {print $2}' $installdir/.env);fi
+					;;
+				phala-pherry)
+					docker image rm $(awk -F "=" 'NR==3 {print $2}' $installdir/.env) 
+					;;
+				khala-node)
+					docker image rm phalanetwork/khala-node
+					if [ -d /var/khala-dev-node ]; then rm -rf /var/khala-dev-node;fi
+					;;
+				*)
+					break
+			esac
 		fi
-		case $container_name in
-			phala-node)
-				docker image rm $(awk -F "=" 'NR==1 {print $2}' $installdir/.env)
-				if [ -d $(awk -F '[=:]' 'NR==4 {print $2}' $installdir/.env) ]; then rm -rf $(awk -F '[=:]' 'NR==4 {print $2}' $installdir/.env);fi
-				;;
-			phala-pruntime)
-				docker image rm $(awk -F "=" 'NR==2 {print $2}' $installdir/.env)
-				if [ -d $(awk -F '[=:]' 'NR==5 {print $2}' $installdir/.env) ]; then rm -rf $(awk -F '[=:]' 'NR==5 {print $2}' $installdir/.env);fi
-				;;
-			phala-pherry)
-				docker image rm $(awk -F "=" 'NR==3 {print $2}' $installdir/.env) 
-				;;
-			khala-node)
-				docker image rm phalanetwork/khala-node
-				if [ -d /var/khala-dev-node ]; then rm -rf /var/khala-dev-node;fi
-				;;
-			phala-pruntime-bench)
-				docker image rm swr.cn-east-3.myhuaweicloud.com/phala/phala-dev-pruntime-bench
-				;;
-			phala-sgx_detect)
-				docker image rm swr.cn-east-3.myhuaweicloud.com/phala/phala-sgx_detect:latest
-				;;
-			*)
-				break
-		esac
 	done
 	log_success "----------成功删数据----------"
 
@@ -88,30 +82,24 @@ function update_noclean()
 		if [ ! -z $(docker container ls -q -f "name=$container_name") ]; then
 			docker container stop $container_name
 			docker container rm --force $container_name
+			case $container_name in
+				phala-node)
+					docker image rm $(awk -F "=" 'NR==1 {print $2}' $installdir/.env)
+					;;
+				phala-pruntime)
+					docker image rm $(awk -F "=" 'NR==2 {print $2}' $installdir/.env)
+					;;
+				phala-pherry)
+					docker image rm $(awk -F "=" 'NR==3 {print $2}' $installdir/.env) 
+					;;
+				khala-node)
+					docker image rm phalanetwork/khala-node
+					if [ -d /var/khala-dev-node ]; then rm -rf /var/khala-dev-node;fi
+					;;
+				*)
+					break
+			esac
 		fi
-		case $container_name in
-			phala-node)
-				docker image rm $(awk -F "=" 'NR==1 {print $2}' $installdir/.env)
-				;;
-			phala-pruntime)
-				docker image rm $(awk -F "=" 'NR==2 {print $2}' $installdir/.env)
-				;;
-			phala-pherry)
-				docker image rm $(awk -F "=" 'NR==3 {print $2}' $installdir/.env) 
-				;;
-			khala-node)
-				docker image rm phalanetwork/khala-node
-				if [ -d /var/khala-dev-node ]; then rm -rf /var/khala-dev-node;fi
-				;;
-			phala-pruntime-bench)
-				docker image rm swr.cn-east-3.myhuaweicloud.com/phala/phala-dev-pruntime-bench
-				;;
-			phala-sgx_detect)
-				docker image rm swr.cn-east-3.myhuaweicloud.com/phala/phala-sgx_detect:latest
-				;;
-			*)
-				break
-		esac
 	done
 
 	start
