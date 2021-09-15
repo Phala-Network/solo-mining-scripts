e690f4f19aa9eaccb7ff48d5d3e0194a4b544179
yifeng <jiangyifeng2016@gmail.com>
2021-09-16 00:07:11 +0800

    Update uninstall.sh

--- scripts/cn/uninstall.sh
+++ scripts/cn/uninstall.sh
@@ -2,7 +2,7 @@
 
 function uninstall()
 {
-	for container_name in phala-node phala-pruntime phala-pherry khala-node phala-pruntime-bench phala-sgx_detect
+	for container_name in phala-node phala-pruntime phala-pherry phala-pruntime-bench phala-sgx_detect
 	do
 		if [ ! -z $(docker container ls -q -f "name=$container_name") ]; then
 			docker container stop $container_name
@@ -18,10 +18,6 @@ function uninstall()
 					;;
 				phala-pherry)
 					docker image rm $(awk -F "=" 'NR==3 {print $2}' $installdir/.env) 
-					;;
-				khala-node)
-					docker image rm phalanetwork/khala-node
-					if [ -d /var/khala-dev-node ]; then rm -rf /var/khala-dev-node;fi
 					;;
 				phala-pruntime-bench)
 					docker image rm phalanetwork/phala-dev-pruntime-bench
