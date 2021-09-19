e1c32a7f1ec94ca9b00faba1b3893f99240529dc
wonderfulstudy <jiangyifeng2016@gmail.com>
2021-09-19 18:59:49 +0800

    `phala uninstall` cannot rm .env file

--- scripts/en/uninstall.sh
+++ scripts/en/uninstall.sh
@@ -31,7 +31,7 @@ function uninstall()
 		fi
 	done
 	remove_dirver
-	rm -rf $installdir/script $installdir/{docker-compose.yml,console.js}
+	rm -rf $installdir/{script,docker-compose.yml,console.js}
 	rm /usr/bin/phala
 
 	log_success "---------------Uninstall phala node sucess---------------"
