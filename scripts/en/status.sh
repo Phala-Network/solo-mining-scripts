839aaa0b33940116f6ad2f6a3c66d44a7cdf7ed3
darknight7 <subscryer@gmail.com>
2021-11-19 11:16:58 +0100

    Update status.sh
    Adding ETA (countdown to full sync) functionality, I did it for myself and I submit it for review in case you want to integrate the functionality in the official scripts. Also made a few consistency changes here and there.

--- scripts/en/status.sh
+++ scripts/en/status.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+function eta() #current block, initial block, time delta
+{
+	local count=$(( $1 - $2 ))
+	if [ ${count} -eq 0 ]; then
+		echo ", ETA still unknown"
+	else
+		count=$(( $1 / ( ${count} / $3 ) ))
+		count=$(eval echo $(date -ud "@${count}" +'$((%s/3600/24))d %Hh %Mm %Ss'))
+		echo ", ETA is ${count}"
+	fi
+}
+
 function status()
 {
 	trap "clear;exit" 2
+	local time_init=0
+	local kusama_ch_init=0
+	local kusama_ph_init=0
+	local khala_ch_init=0
+	local khala_ph_init=0
 	while true; do
 		local node_status="stop"
 		local pruntime_status="stop"
@@ -52,8 +69,8 @@ function status()
 			pherry_status="exited"
 		fi
 
-		SYNCED="Synchronization completed"
-		SYNCING="Synchronizing, please wait"
+		SYNCED="Completed"
+		SYNCING="Please wait"
 
 		blockInfo=(${khala_node_block} ${khala_head_block} ${kusama_node_block} ${kusama_head_block} ${blocknum} ${headernum})
 		compareOrder1=(1 3 0 2)
@@ -82,6 +99,52 @@ function status()
 			gas_balance=$(echo '\E[1;32m'${balance}'\E[0m'"PHA")
 		fi
 
+		local kusama_ch_eta=""
+		local kusama_ph_eta=""
+		local khala_ch_eta=""
+		local khala_ph_eta=""
+		local time_now=0
+		local time_diff=0
+		local kusama_ch_now=0
+		local kusama_ph_now=0
+		local khala_ch_now=0
+		local khala_ph_now=0
+
+		if [ ${time_init} -ne 0 ]; then
+			time_now=$(date +%s)
+			time_diff=$(( ${time_now} - ${time_init} ))
+			kusama_ch_now=${kusama_node_block}
+			kusama_ph_now=${headernum}
+			khala_ch_now=${khala_node_block}
+			khala_ph_now=${blocknum}
+			if [ "${sync_status[0]}" = "${SYNCING}" ]; then #calculate ETA for khala chain
+				khala_ch_eta=$(eta ${khala_ch_now} ${khala_ch_init} ${time_diff})
+			else
+				khala_ch_eta=""
+			fi
+			if [ "${sync_status[1]}" = "${SYNCING}" ]; then #calculate ETA for kusama chain
+				kusama_ch_eta=$(eta ${kusama_ch_now} ${kusama_ch_init} ${time_diff})
+			else
+				kusama_ch_eta=""
+			fi
+			if [ "${sync_status[2]}" = "${SYNCING}" ]; then #calculate ETA for khala pherry
+				khala_ph_eta=$(eta ${khala_ph_now} ${khala_ph_init} ${time_diff})
+			else
+				khala_ph_eta=""
+			fi
+			if [ "${sync_status[3]}" = "${SYNCING}" ]; then #calculate ETA for kusama pherry
+				kusama_ph_eta=$(eta ${kusama_ph_now} ${kusama_ph_init} ${time_diff})
+			else
+				kusama_ph_eta=""
+			fi
+		else
+			time_init=$(date +%s)
+			kusama_ch_init=${kusama_node_block}
+			kusama_ph_init=${headernum}
+			khala_ch_init=${kusama_node_block}
+			khala_ph_init=${headernum}
+		fi
+
 		clear
 		printf "
 ------------------------------ Script version ${script_version} ----------------------------
@@ -92,12 +155,12 @@ function status()
 	phala-pruntime		${pruntime_status}
 	phala-pherry		${pherry_status}			khala ${blocknum} / kusama ${headernum}
 --------------------------------------------------------------------------
-	Status check						result
+	Status check				result (kusama pherry ETA takes a long time to become reliable)
 --------------------------------------------------------------------------
-	khala chain synchronization status		${sync_status[0]}, difference is ${diff[0]}
-	kusama chain synchronization status		${sync_status[1]}, difference is ${diff[1]}
-	pherry synchronizes khala chain status		${sync_status[2]}, difference is ${diff[2]}
-	pherry syncs kusama chain status  		${sync_status[3]}, difference is ${diff[3]}
+	khala chain synchronization status	${sync_status[0]}, difference is ${diff[0]}${khala_ch_eta}
+	kusama chain synchronization status	${sync_status[1]}, difference is ${diff[1]}${kusama_ch_eta}
+	khala pherry synchronization status	${sync_status[2]}, difference is ${diff[2]}${khala_ph_eta}
+	kusama pherry synchronization status	${sync_status[3]}, difference is ${diff[3]}${kusama_ph_eta}
 --------------------------------------------------------------------------
 	account information		content
 --------------------------------------------------------------------------
