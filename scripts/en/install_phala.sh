e232f84b219b1f262584416a32e432915b32c696
David Haunschild <37558304+hauni97@users.noreply.github.com>
2021-11-25 22:01:18 +0100

    added Ubuntu 21.04 support

--- scripts/en/install_phala.sh
+++ scripts/en/install_phala.sh
@@ -188,6 +188,11 @@ elif [ $(lsb_release -r | grep -o "[0-9]*\.[0-9]*") = "20.04" ]; then
 	dcap_driverbin=$(awk -F '/' 'NR==12 {print $NF}' $installdir/.env)
 	isgx_driverurl=$(awk -F '=' 'NR==14 {print $2}' $installdir/.env)
 	isgx_driverbin=$(awk -F '/' 'NR==14 {print $NF}' $installdir/.env)
+elif [ $(lsb_release -r | grep -o "[0-9]*\.[0-9]*") = "21.04" ]; then
+	dcap_driverurl=$(awk -F '=' 'NR==12 {print $2}' $installdir/.env)
+	dcap_driverbin=$(awk -F '/' 'NR==12 {print $NF}' $installdir/.env)
+	isgx_driverurl=$(awk -F '=' 'NR==14 {print $2}' $installdir/.env)
+	isgx_driverbin=$(awk -F '/' 'NR==14 {print $NF}' $installdir/.env)
 else
 	log_err "----------Your system is not supported. Phala currently only supports Ubuntu 18.04/Ubuntu 20.04----------"
 	exit 1
