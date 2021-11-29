30a79231e536d94e951d0e5eb8ceae8d5b71a537
David Haunschild <davidh99@hotmail.de>
2021-11-29 13:25:14 +0100

    CN Ubuntu 21.04/21.10 Support

--- scripts/en/install_phala.sh
+++ scripts/en/install_phala.sh
@@ -199,6 +199,6 @@ elif [ $(lsb_release -r | grep -o "[0-9]*\.[0-9]*") = "21.04" ]; then
 	isgx_driverurl=$(awk -F '=' 'NR==14 {print $2}' $installdir/.env)
 	isgx_driverbin=$(awk -F '/' 'NR==14 {print $NF}' $installdir/.env)
 else
-	log_err "----------Your system is not supported. Phala currently only supports Ubuntu 18.04/Ubuntu 20.04----------"
+	log_err "----------Your system is not supported. Phala currently supports Ubuntu 18.04/Ubuntu 20.04 & limited support for Ubuntu 21.04/21.10 ----------"
 	exit 1
 fi
