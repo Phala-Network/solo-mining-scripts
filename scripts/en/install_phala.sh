60bc5ae262aaa096a42d80ea606ecdba6415c3c8
David Haunschild <37558304+hauni97@users.noreply.github.com>
2022-01-06 17:08:49 +0100

    fix rare docker install error (Ubuntu 18.04)
    https://github.com/Phala-Network/solo-mining-scripts/issues/42

--- scripts/en/install_phala.sh
+++ scripts/en/install_phala.sh
@@ -19,6 +19,7 @@ function install_depenencies()
 						;;
 					docker)
 						curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+						apt install software-properties-common
 						add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 						apt-get install -y docker-ce docker-ce-cli containerd.io
 						usermod -aG docker $USER
